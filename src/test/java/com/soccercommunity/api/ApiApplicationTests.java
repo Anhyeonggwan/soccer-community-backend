6d76cc1fa760a93567dd388cd2606f5409e52e90
Anhyeonggwan <anhyeonggwan@gmail.com>
2025-09-25 21:57:17 +0900

    setting

---
+++ src/test/java/com/soccercommunity/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soccercommunity.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
